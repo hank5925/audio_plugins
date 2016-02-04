19e029f95aeac43e71057a78d452ffbc9fe099c7
hank5925 <hank5925@gmail.com>
2016-02-03 23:59:34 -0800

    add new ignored files

---
+++ Builds/MacOSX/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UnitTestCLT
+//
+//  Created by Ying-Shu Kuo on 1/16/16.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
